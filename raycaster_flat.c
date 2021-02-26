65c1ae15a6211c01df2aaa866834dd2b1ab6c284
Madelaine Hogg <mhogg@et-h4.kzn.21-school.ru>
2021-02-26 21:36:13 +0300

    add struct

--- raycaster_flat.c
+++ raycaster_flat.c
@@ -6,7 +6,7 @@
 /*   By: mhogg <mhogg@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 20:06:15 by mhogg             #+#    #+#             */
-/*   Updated: 2021/02/25 20:30:04 by mhogg            ###   ########.fr       */
+/*   Updated: 2021/02/26 21:35:19 by mhogg            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,58 @@
 #include <math.h>
 #include <mlx.h>
 
-#define screenWidth 640
-#define screenHeight 480
-#define mapWidth 24
-#define mapHeight 24
+#define img_width 640
+#define img_height 480
+#define map_width 24
+#define map_height 24
 
-
-typedef struct  s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+typedef struct  s_data
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
 }               t_data;
 
-typedef struct s_complex
+typedef	struct	s_var
 {
-	int boba;
+	double pos_x;
+	double pos_y;
+	double dir_x;
+	double dir_y;
+	double plane_x;
+	double plane_y;
+
+}				t_var;
+
+typedef struct s_all
+{
+	t_data		*data;
+	t_var		*var;
 	
 	
-}				t_complex;
+}				t_all;
 
+int	close_func(void)
+{
+	exit(0);
+}
 
-void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int		key_hook(int key)
+{
+	if (key == 53)
+		exit(0);
+	return (0);
+}
+
+void	my_mlx_pixel_put(t_all all, int x, int y, int color)
 {
     char    *dst;
 
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    dst = all.data->addr + (y * all.data->line_length + x * (all.data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
 }
 
@@ -52,42 +77,42 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	// mlx_destroy_image(complex->mlx, complex->data.img);
 // 	if (keycode == 12)
 // 	{
-// 		if(worldMap[(int)(posX + dirX * moveSpeed)][(int)(posY)] == 0)
-// 			posX += dirX * moveSpeed;
-// 		if(worldMap[(int)(posX)][(int)(posY + dirY * moveSpeed)] == 0)
-// 			posY += dirY * moveSpeed;
+// 		if(map[(int)(all.var->pos_x + dirX * moveSpeed)][(int)(all.var->pos_y)] == 0)
+// 			all.var->pos_x += dirX * moveSpeed;
+// 		if(map[(int)(all.var->pos_x)][(int)(all.var->pos_y + dirY * moveSpeed)] == 0)
+// 			all.var->pos_y += dirY * moveSpeed;
 // 	}
 // 	if (keycode == 0)
 // 	{
-// 		if(worldMap[(int)(posX - dirX * moveSpeed)][(int)(posY)] == 0)
-// 			posX -= dirX * moveSpeed;
-// 		if(worldMap[(int)(posX)][(int)(posY - dirY * moveSpeed)] == 0)
-// 			posY -= dirY * moveSpeed;
+// 		if(map[(int)(all.var->pos_x - dirX * moveSpeed)][(int)(all.var->pos_y)] == 0)
+// 			all.var->pos_x -= dirX * moveSpeed;
+// 		if(map[(int)(all.var->pos_x)][(int)(all.var->pos_y - dirY * moveSpeed)] == 0)
+// 			all.var->pos_y -= dirY * moveSpeed;
 // 	}
 
 // 	if (keycode == 1)
 // 	{
-// 		if(worldMap[(int)(posX + dirY * moveSpeed)][(int)(posY)] == 0)
-// 			posX += dirY * moveSpeed;
-// 		if(worldMap[(int)(posX)][(int)(posY - dirX * moveSpeed)] == 0)
-// 			posY -= dirX * moveSpeed;
+// 		if(map[(int)(all.var->pos_x + dirY * moveSpeed)][(int)(all.var->pos_y)] == 0)
+// 			all.var->pos_x += dirY * moveSpeed;
+// 		if(map[(int)(all.var->pos_x)][(int)(all.var->pos_y - dirX * moveSpeed)] == 0)
+// 			all.var->pos_y -= dirX * moveSpeed;
 
 // 	}
 // 	if (keycode == 2)
 // 	{
-// 		if(worldMap[(int)(posX - dirY * moveSpeed)][(int)(posY)] == 0)
-// 			posX -= dirY * moveSpeed;
-// 		if(worldMap[(int)(posX)][(int)(posY + dirX * moveSpeed)] == 0)
-// 			posY += dirX * moveSpeed;
+// 		if(map[(int)(all.var->pos_x - dirY * moveSpeed)][(int)(all.var->pos_y)] == 0)
+// 			all.var->pos_x -= dirY * moveSpeed;
+// 		if(map[(int)(all.var->pos_x)][(int)(all.var->pos_y + dirX * moveSpeed)] == 0)
+// 			all.var->pos_y += dirX * moveSpeed;
 // 	}
-	// complex->data.img = mlx_new_image(complex->mlx, screenWidth, screenHeight);
+	// complex->data.img = mlx_new_image(complex->mlx, img_width, img_height);
 	// complex->data.addr = mlx_get_data_addr(complex->data.img, &complex->data.bpp, &complex->data.length, &complex->data.end);
 	// ft_mlx(complex);
 	// mlx_put_image_to_window(complex->mlx, complex->mlx_win, complex->data.img, 0, 0);
 // }
 
 
-int worldMap[mapWidth][mapHeight]=
+int map[map_width][map_height]=
 {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -115,33 +140,27 @@ int worldMap[mapWidth][mapHeight]=
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
-void		ft_mlx(t_data data)
+void		ft_mlx(t_all all)
 {
 	double deltaDistX;
 	double deltaDistY;
-	double posX = 22, posY = 12;  //x and y start position
-	double dirX = -1, dirY = 0; //initial direction vector
-	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
 
-	double time = 0; //time of current frame
-	double oldTime = 0; //time of previous frame
-
-	for(int x = 0; x < screenWidth; x++)
+	for(int x = 0; x < img_width; x++)
     {
  		//calculate ray position and direction
- 		double cameraX = 2 * x / (double)screenWidth - 1; //x-coordinate in camera space
- 		double rayDirX = dirX + planeX * cameraX;
- 		double rayDirY = dirY + planeY * cameraX;
+ 		double cameraX = 2 * x / (double)img_width - 1; //x-coordinate in camera space
+ 		double rayDirX = all.var->dir_x + all.var->plane_x * cameraX;
+ 		double rayDirY = all.var->dir_y + all.var->plane_y * cameraX;
  		//which box of the map we're in
- 		int mapX = (int)posX;
- 		int mapY = (int)posY;
+ 		int mapX = (int)all.var->pos_x;
+ 		int mapY = (int)all.var->pos_y;
 
-		  //length of ray from current position to next x or y-side
+		//length of ray from current position to next x or y-side
 		double sideDistX;
 		double sideDistY;
 
        //length of ray from one x or y-side to next x or y-side
-		                                                                                                                                                                                                                                                                                                                                                                                                                                          deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+		deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
 		deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
 
 
@@ -157,22 +176,22 @@ void		ft_mlx(t_data data)
 	if(rayDirX < 0)
 	{
 		stepX = -1;
-		sideDistX = (posX - mapX) * deltaDistX;
+		sideDistX = (all.var->pos_x - mapX) * deltaDistX;
 	}
 	else
 	{
 		stepX = 1;
-		sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+		sideDistX = (mapX + 1.0 - all.var->pos_x) * deltaDistX;
 	}
 	if(rayDirY < 0)
 	{
 		stepY = -1;
-		sideDistY = (posY - mapY) * deltaDistY;
+		sideDistY = (all.var->pos_y - mapY) * deltaDistY;
 	}
 	else
 	{
 		stepY = 1;
-		sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+		sideDistY = (mapY + 1.0 - all.var->pos_y) * deltaDistY;
 	}
 	//perform DDA
 	while (hit == 0)
@@ -191,51 +210,62 @@ void		ft_mlx(t_data data)
 			side = 1;
 		}
 		//Check if ray has hit a wall
-		if(worldMap[mapX][mapY] > 0)			// запомните твари 
+		if(map[mapX][mapY] > 0)			// запомните твари 
 			hit = 1;
 	}
       //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
 	if(side == 0)
-		perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+		perpWallDist = (mapX - all.var->pos_x + (1 - stepX) / 2) / rayDirX;
 	else
-		perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+		perpWallDist = (mapY - all.var->pos_y + (1 - stepY) / 2) / rayDirY;
 
       //Calculate height of line to draw on screen
-	int lineHeight = (int)(screenHeight / perpWallDist);
+	int lineHeight = (int)(img_height / perpWallDist);
 	
 	//calculate lowest and highest pixel to fill in current stripe
-	int drawStart = -lineHeight / 2 + screenHeight / 2;
+	int drawStart = -lineHeight / 2 + img_height / 2;
 	if(drawStart < 0)
 		drawStart = 0;
-	int drawEnd = lineHeight / 2 + screenHeight / 2;
-	if(drawEnd >= screenHeight)
-		drawEnd = screenHeight - 1;
+	int drawEnd = lineHeight / 2 + img_height / 2;
+	if(drawEnd >= img_height)
+		drawEnd = img_height - 1;
 	int y = 0;
 	
 	float moveSpeed = 0.3;
 	
-		while (y < screenHeight)
+		while (y < img_height)
 		{
 			if (y >= drawStart && y <= drawEnd)
-				my_mlx_pixel_put(&data, x, y, 0xFFFFFF);
+				my_mlx_pixel_put(all, x, y, 0xFFFFFF);
 			y++;
 		}
-	}
-	
+	}	
 }
 
 int			main(void)
 {
-	t_data data;
-	void	*mlx;
-	void	*mlx_win;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, screenWidth, screenHeight, "cub3D");
-	data.img = mlx_new_image(mlx, screenWidth, screenHeight);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	t_data	data;
+	t_var	var;
+	t_all	all;
 	
-	ft_mlx(data);
-	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
+	all.data = &data;
+	all.var = &var;
+	all.var->pos_x = 22;
+	all.var->pos_y = 12;
+	all.var->dir_x = -1;
+	all.var->dir_y = 0;
+	all.var->plane_x = 0;
+	all.var->plane_y = 0.66;
+	
+	all.data->mlx_ptr = mlx_init();
+	all.data->win_ptr = mlx_new_window(all.data->mlx_ptr, img_width, img_height, "cub3D");
+	all.data->img = mlx_new_image(all.data->mlx_ptr, img_width, img_height);
+	all.data->addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	
+	ft_mlx(all);
+	mlx_put_image_to_window(all.data->mlx_ptr, all.data->win_ptr, all.data->img, 0, 0);
 	write(1, "open\n", 5);
-	mlx_loop(mlx);	
+	mlx_key_hook(all.data->win_ptr, key_hook, 0); // exit on ESC
+	mlx_hook(all.data->win_ptr, 17, 1L<<0, close_func, 0); 
+	mlx_loop(all.data->mlx_ptr);
 }
