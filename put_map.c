d2a05bb4830d53c900ec95f5ebb7b9fd30cbec86
Madelaine Hogg <mhogg@er-d5.kzn.21-school.ru>
2021-02-26 12:44:04 +0300

    put to struct

--- put_map.c
+++ put_map.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   put_square.c                                       :+:      :+:    :+:   */
+/*   put_map.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhogg <mhogg@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 11:44:01 by mhogg             #+#    #+#             */
-/*   Updated: 2021/02/26 00:52:49 by mhogg            ###   ########.fr       */
+/*   Updated: 2021/02/26 12:43:00 by mhogg            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ typedef struct  s_var
 	int			sq_size;
 	int			img_width;
 	int			img_height;
+	int			color;
 }				t_var;
 
 typedef struct  s_color
@@ -111,26 +112,26 @@ float	grad_to_radian(int grad)
 	return (M_PI * grad / 180);
 }
 
-void	put_square(t_img *image, t_var *var, int x, int y, int color)
+void	put_square(t_global *global, int x, int y, int color)
 {
 	int	x_count;
 	int	y_count;
 	
 	y_count = 0;
 	
-	while (y_count < var->sq_size)
+	while (y_count < global->var->sq_size)
 	{
 		x_count = 0;
-		while(x_count < var->sq_size)
+		while(x_count < global->var->sq_size)
 		{
-				my_mlx_pixel_put(image, x_count + x, y_count + y, color); 
+				my_mlx_pixel_put(global->image, x_count + x, y_count + y, color); 
 			x_count++;
 	 	}
 		y_count++;
 	}
 }
 
-void	put_map(t_img *image, t_var *var, int color)
+void	put_map(t_global *global, int color)
 {
 	int	i;
 	int	j;
@@ -144,7 +145,7 @@ void	put_map(t_img *image, t_var *var, int color)
 		while (i < map_width)
 		{
 			if (map[i][j] == 1 || map[i][j] == 2 || map[i][j] == 3 || map[i][j] == 4 || map[i][j] == 5)
-				put_square(image, var, i * var->sq_size, j * var->sq_size, color);
+				put_square(global, i * global->var->sq_size, j * global->var->sq_size, color);
 				//my_mlx_pixel_put(image, i, j, color);
 			i++;
 		}
@@ -154,25 +155,25 @@ void	put_map(t_img *image, t_var *var, int color)
 
 int		main(void)
 {
-	int			x;
-	int			y;
-	float		angle;
+	int			x, y, r, g, b;
+	double		angle;
 	t_var		var;
 	t_img		image;
 	t_global	global;
 	
+	global.var = &var;
+	global.image = &image;
+	
 	var.sq_size = 20;
 	var.img_width = 640;
 	var.img_height = 480;
-	
+	r = 255;
+	g = 255;
+	b = 255;
+	var.color = create_trgb(0, r, g, b);
 	x = 100;
 	y = 20;
-	angle = 30;
-	int r = 255;
-	int g = 255;
-	int b = 255;
-	
-	int color = create_trgb(0, r, g, b);
+	angle = 30;	
 
 	image.mlx_ptr = mlx_init(); // Initializes the mlx library. Will return NULL if initialization failed.
 	image.win_ptr = mlx_new_window(image.mlx_ptr, var.img_width, var.img_height, "cub3D"); 
@@ -181,7 +182,7 @@ int		main(void)
                                  &image.endian); 
 	
 	//put_square(&image, &var, x, y, color);
-	put_map(&image, &var, color);
+	put_map(&global, var.color);
 	
 	mlx_put_image_to_window(image.mlx_ptr, image.win_ptr, image.img_ptr, 0, 0);
 	mlx_key_hook(image.win_ptr, key_hook, 0); // exit on ESC
